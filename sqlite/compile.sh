1cfe9981c23ab173bb8ba38ddf01666bc19e3cb9
Sundararajan Athijegannathan <sundararajan.athijegannathan@oracle.com>
2020-06-03 22:53:34 +0530

    adding sqlite3 sample

---
+++ sqlite/compile.sh
@@ -0,0 +1,4 @@
+jextract \
+  -I /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/sqlite3.h \
+  -t org.sqlite -lsqlite3 
---
+++ sqlite/run.sh
@@ -0,0 +1,4 @@
+java -Dforeign.restricted=permit \
+   --add-modules jdk.incubator.foreign \
+   -Djava.library.path=/usr/lib SqliteMain.java
+
