83c3f6b524bf41c5909fc1b1b6015f2c2f1d3c46
Sundararajan Athijegannathan <sundararajan.athijegannathan@oracle.com>
2020-07-20 21:03:28 +0530

    Added -XstartOnFirstThread option for opengl sample

--- opengl/run.sh
+++ opengl/run.sh
@@ -1,2 +1,2 @@
-java -Dforeign.restricted=permit --add-modules jdk.incubator.foreign \
+java -XstartOnFirstThread -Dforeign.restricted=permit --add-modules jdk.incubator.foreign \
     -Djava.library.path=.:/System/Library/Frameworks/OpenGL.framework/Versions/Current/Libraries/ Teapot.java $*
