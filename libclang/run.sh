e4dc0862000e08ed426ef4323854782eaf85357e
Sundararajan Athijegannathan <sundararajan.athijegannathan@oracle.com>
2020-12-10 15:31:02 +0530

    Adding -Djava.library.path for libclang sample (so that the sample can run on jdk16)

--- libclang/run.sh
+++ libclang/run.sh
@@ -1,2 +1,2 @@
-java -Dforeign.restricted=permit --add-modules jdk.incubator.foreign \
+java -Djava.library.path=${LIBCLANG_HOME}/lib -Dforeign.restricted=permit --add-modules jdk.incubator.foreign \
     ASTPrinter.java $*
