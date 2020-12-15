d6b95124732a0ca0dd4cab420b11f6ebbdeda889
Sundararajan Athijegannathan <sundararajan.athijegannathan@oracle.com>
2020-12-15 09:57:40 +0530

    tensorflow sample's compile.sh uses --source

--- tensorflow/compile.sh
+++ tensorflow/compile.sh
@@ -1,8 +1,6 @@
-jextract --source \
+jextract \
   -I /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/ \
   -t org.tensorflow \
   -I ${LIBTENSORFLOW_HOME}/include \
   -l ${LIBTENSORFLOW_HOME}/lib/libtensorflow.dylib \
   ${LIBTENSORFLOW_HOME}/include/tensorflow/c/c_api.h
-
-javac --add-modules jdk.incubator.foreign org/tensorflow/*.java
