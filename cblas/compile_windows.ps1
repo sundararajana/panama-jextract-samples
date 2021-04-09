2f58c39ccf7639145528071dafe5875c0ddb797c
Jorn Vernee <jbvernee@xs4all.nl>
2021-04-09 19:01:05 +0200

    Remove most filtering

--- cblas/compile_windows.ps1
+++ cblas/compile_windows.ps1
@@ -8,17 +8,8 @@ param(
 $jextract = find-tool("jextract")
 
 & $jextract `
-  -I "$blasPath\include" `
-  --dump-includes 'includes_all.conf' `
-  -- `
-  "$blasPath\include\cblas.h"
-  
-filter_file 'includes_all.conf' 'cblas.h' 'includes_filtered.conf'
-
-& $jextract `
   -t blas `
   -I "$blasPath\include" `
   -l libcblas `
-  '@includes_filtered.conf' `
   -- `
   "$blasPath\include\cblas.h"
--- opengl/compile_windows.ps1
+++ opengl/compile_windows.ps1
@@ -9,18 +9,9 @@ $jextract = find-tool("jextract")
 
 & $jextract `
   -I "$freeglutPath\include" `
-  --dump-includes 'includes_all.conf' `
-  -- `
-  "$freeglutPath\include\GL\glut.h"
-  
-filter_file 'includes_all.conf' '(GL|GLU)' 'includes_filtered.conf'
-
-& $jextract `
-  -I "$freeglutPath\include" `
   "-l" opengl32 `
   "-l" glu32 `
   "-l" freeglut `
   "-t" "opengl" `
-  '@includes_filtered.conf' `
   "--" `
   "$freeglutPath\include\GL\glut.h"
