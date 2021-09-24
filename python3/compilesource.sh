a43a207297df98485b2372c8ce429c7526a6927a
Sundararajan Athijegannathan <sundararajan.athijegannathan@oracle.com>
2021-09-24 18:54:18 +0530

    fixed compilesource.sh of Python3 sample (python version)

--- python3/compilesource.sh
+++ python3/compilesource.sh
@@ -1,9 +1,9 @@
 ANACONDA3_HOME=/opt/anaconda3
 
-jextract --source -l python3.7m \
+jextract --source -l python3.8 \
   -I /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include \
-  -I ${ANACONDA3_HOME}/include/python3.7m \
+  -I ${ANACONDA3_HOME}/include/python3.8 \
   -t org.python \
-  ${ANACONDA3_HOME}/include/python3.7m/Python.h
+  ${ANACONDA3_HOME}/include/python3.8/Python.h
 
 javac --add-modules jdk.incubator.foreign org/python/*.java
