0bfd2e966fae4a4d73546e88a2147d9de5c015d4
Sundararajan Athijegannathan <sundararajan.athijegannathan@oracle.com>
2022-01-31 09:22:13 +0530

    updated libffmpeg version. removed python 2 binding

--- python/compile.sh
+++
@@ -1,7 +0,0 @@
-jextract -l python2.7 \
-  -I /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include \
-  -I /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/python2.7/ \
-  -t org.python \
-   /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/python2.7/Python.h 
-
-
--- python/run.sh
+++
@@ -1,3 +0,0 @@
-java --enable-native-access=ALL-UNNAMED --add-modules jdk.incubator.foreign \
-    -Djava.library.path=/System/Library/Frameworks/Python.framework/Versions/2.7/lib \
-    PythonMain.java
